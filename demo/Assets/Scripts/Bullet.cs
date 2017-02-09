783006c244f490d57b15947f4abdb0cc41147ad4
Grenon Roxanne <roxanne.grenon@cegepmontpetit.ca>
2017-02-09 14:10:34 -0500

    Try first push

---
+++ demo/Assets/Scripts/Bullet.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+	void OnCollisionEnter(Collision col)
+	{
+		if(col.gameObject.tag == "Pickup")
+		{
+			Destroy(col.gameObject);
+			Destroy(gameObject);
+		}
+	}
+}
---
+++ demo/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	[SerializeField]
+	GameObject bulletPrefab;
+
+	[SerializeField]
+	Transform bulletSpawn;
+
+	void Update()
+	{
+		float h = Input.GetAxis("Horizontal");
+		float v = Input.GetAxis("Vertical");
+
+		transform.Rotate(0, h * Time.deltaTime * 150.0f, 0);
+		transform.Translate(v * Time.deltaTime * 3.0f, 0, 0);
+
+		if(Input.GetKeyDown(KeyCode.Space)) 
+		{
+			Fire();
+		}
+	}
+
+	void Fire()
+	{
+		var bullet = (GameObject)Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation);
+		bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 6;
+		Destroy(bullet, 2.0f);
+	}
+}
